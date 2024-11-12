dd187a3d3cc78af6a714ad4831f357c1480cfdc2
Yacine MENNAA <yacinemennaa@gmail.com>
2024-11-12 14:19:28 +0100

    quete10

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,19 +1,49 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Article } from '../../models/article.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './article-page.component.html',
-  styleUrl: './article-page.component.scss'
+  styleUrls: ['./article-page.component.scss']
 })
-export class ArticlePageComponent {
-  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+export class ArticlePageComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly http = inject(HttpClient);
+
   articleId!: number;
+  article!: Article;
+
+  ARTICLE_URL = 'http://localhost:3000/articles';
+  
+  getArticleById(id: number) {
+    this.http.get<Article>(`${this.ARTICLE_URL}/${id}`).subscribe({
+      next: (data) => {
+        this.article = data;
+      },
+      error: (error) => {
+        console.error('Error fetching article:', error);
+      },
+      complete: () => {
+        console.log('Article fetch complete');
+      }
+    });
+  }
+  
+  handleLike() {
+    this.article.isLiked = !this.article.isLiked;
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
+      if (this.articleId) {
+        this.getArticleById(this.articleId);
+      }
     });
   }
 }
