9224b4f406f3d2e0203de93b56ec4452571286d2
Yacine MENNAA <yacinemennaa@gmail.com>
2024-11-12 17:11:40 +0100

    quete11

--- src/app/components/article-detail/article-detail.component.ts
+++ src/app/components/article-detail/article-detail.component.ts
@@ -3,42 +3,31 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Article } from '../../models/article.model';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-detail',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './article-detail.component.html',
-  styleUrls: ['./article-detail.component.scss']
+  styleUrl: './article-detail.component.scss'
 })
 export class ArticleDetailComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly apiService = inject(ApiService);
 
-  private readonly articleSubject = new BehaviorSubject<Article | null>(null);
-  article$: Observable<Article | null> = this.articleSubject.asObservable();
-
   articleId!: number;
+  article$!: Article;
 
   handleLike() {
-    const article = this.articleSubject.value;
-    if (article) {
-      article.isLiked = !article.isLiked;
-      this.apiService.updateArticle$(article).pipe(
-        switchMap((updatedArticle) => {
-          return this.apiService.getArticleById$(updatedArticle.id);
-        })
-      ).subscribe({
-        next: (updatedArticle) => {
-          this.articleSubject.next(updatedArticle);
-        },
-        error: (error) => {
-          console.error('Erreur lors de la mise à jour de l\'article:', error);
-        }
-      });
-    }
+    this.article$.isLiked = !this.article$.isLiked;
+    this.apiService.updateArticle$(this.article$).subscribe({
+      next: (article) => {
+        this.article$ = article;
+      },
+      error: (error) => {
+        console.error('Erreur lors de la mise à jour de l\'article:', error);
+      }
+    });
   }
 
   ngOnInit() {
@@ -47,7 +36,7 @@ export class ArticleDetailComponent implements OnInit {
       if (this.articleId) {
         this.apiService.getArticleById$(this.articleId).subscribe({
           next: (article) => {
-            this.articleSubject.next(article);
+            this.article$ = article;
           },
           error: (error) => {
             console.error('Erreur lors de la récupération de l\'article:', error);
